fb866fbeb9e0eaddf245d02719cae9641718a5ab
bacaron <bacaron@imail.iu.edu>
2017-11-02 12:06:24 -0400

    Update niftiHeaderAmendNODDI.m

--- niftiHeaderAmendNODDI.m
+++ niftiHeaderAmendNODDI.m
@@ -3,7 +3,7 @@ function niftiHeaderAmmendNODDI()
 icvf = niftiRead(fullfile('AMICO','NODDI','FIT_ICVF.nii.gz'));
 od = niftiRead(fullfile('AMICO','NODDI','FIT_OD.nii.gz'));
 isovf = niftiRead(fullfile('AMICO','NODDI','FIT_ISOVF.nii.gz'));
-b0 = niftiRead(fullfile('nodif_acpc_brain.nii.gz'));
+b0 = niftiRead(fullfile('nodif_brain.nii.gz'));
 n_fields_icvf = fieldnames( icvf );
 n_fields_od = fieldnames( od );
 n_fields_isovf = fieldnames (isovf);
